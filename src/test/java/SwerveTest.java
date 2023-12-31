210ae9e2c24d149cce19ae0662221aea80b3eb6f
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-12-31 16:18:56 -0500

    Fix tests

--- src/test/java/SwerveTest.java
+++ src/test/java/SwerveTest.java
@@ -16,22 +16,22 @@ public class SwerveTest {
 		var expectedStates = new SwerveModuleState[4];
 
 		// Go Northwest
-		var nwStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(1, 1, 0), false);
+		var nwStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(1, 1, 0));
 		Arrays.fill(expectedStates, new SwerveModuleState(1, Rotation2d.fromDegrees(45)));
 		assertArrayEquals(expectedStates, nwStates);
 
 		// Go Northeast
-		var neStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(1, -1, 0), false);
+		var neStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(1, -1, 0));
 		Arrays.fill(expectedStates, new SwerveModuleState(1, Rotation2d.fromDegrees(-45)));
 		assertArrayEquals(expectedStates, neStates);
 
 		// Go Southwest
-		var swStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(-1, 1, 0), false);
+		var swStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(-1, 1, 0));
 		Arrays.fill(expectedStates, new SwerveModuleState(1, Rotation2d.fromDegrees(135)));
 		assertArrayEquals(expectedStates, swStates);
 
 		// Go Southwest
-		var seStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(-1, -1, 0), false);
+		var seStates = driveSubsystem.calculateModuleStates(new ChassisSpeeds(-1, -1, 0));
 		Arrays.fill(expectedStates, new SwerveModuleState(1, Rotation2d.fromDegrees(225)));
 		assertArrayEquals(expectedStates, seStates);
 	}
