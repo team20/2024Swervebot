c30731cc2d26c62c63eb03f884137f26c54e46dc
Hwang1907 <69113243+Hwang1907@users.noreply.github.com>
2024-01-18 18:24:18 -0500

    updated drive commands

---
+++ src/main/java/frc/aster/commands/drive/DriveDistanceCommand.java
@@ -0,0 +1,162 @@
+package frc.aster.commands.drive;
+
+import java.util.function.Supplier;
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.aster.subsystems.DriveSubsystem;
+
+/**
+ * The {@code DriveDistanceCommand} is responsible for moving the robot by a
+ * specified distance. It utilizes two
+ * {@code ProfiledPIDController}s to precisely control the let and right wheels.
+ * 
+ * @author Andrew Hwang (u.andrew.h@gmail.com)
+ * @author Jeong-Hyon Hwang (jhhbrown@gmail.com)
+ */
+public class DriveDistanceCommand extends Command {
+
+	/**
+	 * The
+	 * {@code Supplier<Double>) that calculates the target distance in meters that the robot should move (executed
+	 * when the {@code DriveDistanceCommand} is submitted to the scheduler and
+	 * initialized for execution).
+	 */
+	private Supplier<Double> m_targetDistanceCalculator;
+
+	/**
+	 * The target distance in meters that the robot should move.
+	 */
+	private double m_targetDistance;
+
+	/**
+	 * The {@code ProfiledPIDController} for controlling the left wheels.
+	 */
+	private ProfiledPIDController m_leftController;
+
+	/**
+	 * The {@code ProfiledPIDController} for controlling the right wheels.
+	 */
+	private ProfiledPIDController m_rightController;
+
+	/**
+	 * The left encoder position when this {@code DriveDistanceCommand} is
+	 * initialized.
+	 */
+	private double m_startLeftEncoderPosition;
+
+	/**
+	 * The right encoder position when this {@code DriveDistanceCommand} is
+	 * initialized.
+	 */
+	private double m_startRightEncoderPosition;
+
+	/**
+	 * Constructs a new {@code DriveDistanceCommand} whose purpose is to move the
+	 * robot by the specified distance.
+	 * 
+	 * @param targetDistance
+	 *                          the target distance in meters that the robot should
+	 *                          move
+	 * @param distanceTolerance
+	 *                          the distance error which is tolerable
+	 */
+	public DriveDistanceCommand(double targetDistance, double distanceTolerance) {
+		this(() -> targetDistance, distanceTolerance);
+	}
+
+	/**
+	 * Constructs a new {@code DriveDistanceCommand} whose purpose is to move the
+	 * robot by the specified distance.
+	 * 
+	 * @param targetDistanceCalculator
+	 *                                 a {@code Supplier<Double>} that calculates
+	 *                                 the target distance in meters that the robot
+	 *                                 should
+	 *                                 move (executed when the
+	 *                                 {@code DriveDistanceCommand} is submitted to
+	 *                                 the
+	 *                                 scheduler and initialized for execution)
+	 * 
+	 * @param distanceTolerance        the distance error which is tolerable
+	 */
+	public DriveDistanceCommand(Supplier<Double> targetDistanceCalculator, double distanceTolerance) {
+		m_targetDistanceCalculator = targetDistanceCalculator;
+		double kP = .3, kI = 0.0, kD = 0.0;
+		var constraints = new TrapezoidProfile.Constraints(3, 2);
+		m_leftController = new ProfiledPIDController(kP, kI, kD, constraints);
+		m_rightController = new ProfiledPIDController(kP, kI, kD, constraints);
+		m_leftController.setTolerance(distanceTolerance);
+		m_rightController.setTolerance(distanceTolerance);
+		addRequirements(DriveSubsystem.get());
+	}
+
+	/**
+	 * Is invoked whenever this {@code DriveDistanceCommand} is initially scheduled
+	 * for execution.
+	 */
+	@Override
+	public void initialize() {
+		m_startLeftEncoderPosition = DriveSubsystem.get().getLeftEncoderPosition();
+		m_startRightEncoderPosition = DriveSubsystem.get().getRightEncoderPosition();
+		try {
+			m_targetDistance = m_targetDistanceCalculator.get();
+		} catch (Exception e) {
+			m_targetDistance = 0;
+		}
+		SmartDashboard.putString(
+				"drive:initialize",
+				String.format("left encoder position: %.1f, right encoder position: %.1f, target distance: %.1f",
+						m_startLeftEncoderPosition,
+						m_startRightEncoderPosition, m_targetDistance));
+	}
+
+	/**
+	 * Is invoked periodically while this {@code DriveDistanceCommand} is scheduled.
+	 */
+	@Override
+	public void execute() {
+		var leftEncoderPosition = DriveSubsystem.get().getLeftEncoderPosition();
+		var rightEncoderPosition = DriveSubsystem.get().getRightEncoderPosition();
+		double leftSpeed = m_leftController.calculate(leftEncoderPosition - m_startLeftEncoderPosition,
+				m_targetDistance);
+		double rightSpeed = m_rightController.calculate(rightEncoderPosition - m_startRightEncoderPosition,
+				m_targetDistance);
+		DriveSubsystem.get().tankDrive(leftSpeed, rightSpeed);
+		SmartDashboard.putString(
+				"drive:execute",
+				String.format(
+						"left encoder position: %.1f, right encoder position: %.1f, left speed: %.1f, right speed: %.1f",
+						leftEncoderPosition, rightEncoderPosition,
+						leftSpeed, rightSpeed));
+	}
+
+	/**
+	 * Is invoked once this {@code DriveDistanceCommand} is ended or interrupted.
+	 * 
+	 * @param interrupted
+	 *                    indicates if this {@code DriveDistanceCommand} was
+	 *                    interrupted
+	 */
+	@Override
+	public void end(boolean interrupted) {
+		DriveSubsystem.get().tankDrive(0, 0);
+	}
+
+	/**
+	 * Determines whether or not this {@code DriveDistanceCommand} needs to end.
+	 * 
+	 * @return {@code true} if this {@code DriveDistanceCommand} needs to end;
+	 *         {@code false} otherwise
+	 */
+	@Override
+	public boolean isFinished() {
+		return m_leftController.atSetpoint() && m_rightController.atSetpoint();
+	}
+}
--- src/main/java/frc/aster/commands/drive/TurnCommand.java
+++ src/main/java/frc/aster/commands/drive/TurnCommand.java
@@ -1,53 +1,116 @@
 package frc.aster.commands.drive;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.aster.Constants.DriveConstants;
 import frc.aster.subsystems.DriveSubsystem;
 
+/**
+ * The {@code TurnCommand} rotates the robot counter-clockwise to a specific
+ * angle relative to its current heading. It
+ * utilizes a {@code PIDController} to maintain precision in reaching the target
+ * angle.
+ * 
+ * @author Andrew Hwang (u.andrew.h@gmail.com)
+ * @author Jeong-Hyon Hwang (jhhbrown@gmail.com)
+ */
 public class TurnCommand extends Command {
-	/** Creates a new TurnCommand. */
-	private double m_targetAngle;
-	private PIDController m_turnController = new PIDController(DriveConstants.kTurnP, DriveConstants.kTurnI,
-			DriveConstants.kTurnD);
 
-	/** Creates a new TurnCommand. */
+	/**
+	 * {@code Supplier<Double>} that calculates the target angle in degrees by which
+	 * the robot should rotate
+	 * counter-clockwise (executed when the {@code TurnCommand} is submitted to the
+	 * scheduler and initialized for
+	 * execution)
+	 */
+	private Supplier<Double> m_targetAngleCalculator;
+
+	/**
+	 * The {@code PIDController} for controlling the rotation.
+	 */
+	private PIDController m_turnController;
+
+	/**
+	 * Constructs a {@code TurnCommand}.
+	 * 
+	 * @param targetAngle
+	 *                    the target angle in degrees by which the robot should
+	 *                    rotate counter-clockwise
+	 */
 	public TurnCommand(double targetAngle) {
-		m_targetAngle = targetAngle;
+		this(() -> targetAngle);
+	}
+
+	/**
+	 * Constructs a {@code TurnCommand}.
+	 * 
+	 * @param targetAngleCalculator
+	 *                              a {@code Supplier<Double>} that calculates the
+	 *                              target angle in degrees by which the robot
+	 *                              should
+	 *                              rotate counter-clockwise (executed when the
+	 *                              {@code TurnCommand} is submitted to the
+	 *                              scheduler and
+	 *                              initialized for execution)
+	 */
+	public TurnCommand(Supplier<Double> targetAngleCalculator) {
+		m_targetAngleCalculator = targetAngleCalculator;
+		m_turnController = new PIDController(DriveConstants.kTurnP, DriveConstants.kTurnI, DriveConstants.kTurnD);
+		m_turnController.setTolerance(DriveConstants.kTurnTolerance);
+		m_turnController.enableContinuousInput(-180, 180);
 		addRequirements(DriveSubsystem.get());
 	}
 
-	// Called when the command is initially scheduled.
+	/**
+	 * Is invoked whenever this {@code TurnCommand} is initially scheduled for
+	 * execution.
+	 */
 	@Override
 	public void initialize() {
-		DriveSubsystem.get().zeroHeading();
-		m_turnController.setSetpoint(m_targetAngle);
-		m_turnController.enableContinuousInput(-180, 180);
-		m_turnController.setTolerance(DriveConstants.kTurnTolerance);
+		// DriveSubsystem.get().zeroHeading();
 		// m_turnController.setIntegratorRange(-0.05, 0.05);
+		double setPoint = DriveSubsystem.get().getHeading();
+		try {
+			setPoint += m_targetAngleCalculator.get();
+		} catch (Exception e) {
+		}
+		m_turnController.setSetpoint(setPoint);
+		SmartDashboard.putString("turn:initialize", String.format("set point: %.1f", setPoint));
 	}
 
-	// Called every time the scheduler runs while the command is scheduled.
+	/**
+	 * Is invoked periodically while this {@code TurnCommand} is scheduled.
+	 */
 	@Override
 	public void execute() {
-		double currAngle = DriveSubsystem.get().getHeading();
-		double turnSpeed = m_turnController.calculate(currAngle);
-
-		// if turn speed is less than 0.1 make it 0.1 in the right direction
+		double heading = DriveSubsystem.get().getHeading();
+		double turnSpeed = m_turnController.calculate(heading);
+		// force the turn speed to be either <= -0.15 or >= 0.15
 		turnSpeed = Math.abs(turnSpeed) < 0.15 ? Math.signum(turnSpeed) * 0.15 : turnSpeed;
-
-		SmartDashboard.putNumber("Heading", currAngle);
 		DriveSubsystem.get().tankDrive(-turnSpeed, turnSpeed);
+		SmartDashboard.putString("turn:execute", String.format("heading: %.1f, turn speed: %.1f", heading, turnSpeed));
 	}
 
-	// Called once the command ends or is interrupted.
+	/**
+	 * Is invoked once this {@code TurnCommand} is ended or interrupted.
+	 * 
+	 * @param interrupted
+	 *                    indicates if this {@code TurnCommand} was interrupted
+	 */
 	@Override
 	public void end(boolean interrupted) {
 		DriveSubsystem.get().tankDrive(0, 0);
 	}
 
-	// Returns true when the command should end.
+	/**
+	 * Determines whether or not this {@code TurnCommand} needs to end.
+	 * 
+	 * @return {@code true} if this {@code TurnCommand} needs to end; {@code false}
+	 *         otherwise
+	 */
 	@Override
 	public boolean isFinished() {
 		return m_turnController.atSetpoint();
